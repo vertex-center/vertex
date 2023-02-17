0d874b449157f6a49a4aaa74dd25c311a9359199
Quentin Guidée <quentin.guidee@gmail.com>
2023-02-16 23:01:18 -0500

    Create project
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/quentinguidee/installer-service
+
+go 1.19
---
+++ services.go
@@ -0,0 +1,7 @@
+package main
+
+type Service struct {
+	id           string
+	name         string
+	dependencies []string
+}
