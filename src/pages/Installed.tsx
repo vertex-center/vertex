d22ad6ed2a3f6b593fc478d09d5783ad6ef23c25
Quentin Guidée <quentin.guidee@gmail.com>
2023-02-23 21:24:15 -0500

    Create example application component

--- src/pages/Installed.tsx
+++ src/pages/Installed.tsx
@@ -1,3 +1,17 @@
+function Application() {
+    return (
+        <div className="flex flex-col rounded-md bg-zinc-100 px-4 py-2">
+            <h3 className="text-lg font-medium">Vertex Spotify</h3>
+        </div>
+    );
+}
+
 export default function Installed() {
-    return <div>Soon.</div>;
+    return (
+        <div className="flex flex-col border-separate border-amber-500 gap-4 m-4">
+            <Application />
+            <Application />
+            <Application />
+        </div>
+    );
 }
