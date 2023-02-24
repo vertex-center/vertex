1a72547b18edf43278ca62616d94e2e115042019
Quentin Guidée <quentin.guidee@gmail.com>
2023-02-23 20:55:30 -0500

    Improve header design

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 function App() {
     return (
-        <div className="App">
+        <div className="bg-zinc-50 min-h-screen">
             <Header />
         </div>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,33 @@
-export default function Header() {
+import { PropsWithChildren } from "react";
+
+function Logo() {
     return (
-        <div className="flex items-center gap-4 flex-center block px-6 py-4">
+        <a
+            href="/"
+            className="flex flex-row items-center gap-3 px-3 py-2 rounded-md hover:bg-zinc-200"
+        >
             <img width={30} src="/images/logo.png" alt="Logo" />
             <h1 className="text-xl font-medium">Vertex</h1>
+        </a>
+    );
+}
+
+function Item({ children }: PropsWithChildren) {
+    return (
+        <li className="rounded-md px-3 py-1 cursor-pointer hover:bg-zinc-200">
+            {children}
+        </li>
+    );
+}
+
+export default function Header() {
+    return (
+        <div className="flex items-center flex-center block px-3 py-2 border-b text-zinc-900 border-zinc-100">
+            <Logo />
+            <ul className="flex flex-row ml-4">
+                <Item>Marketplace</Item>
+                <Item>Installed</Item>
+            </ul>
         </div>
     );
 }
