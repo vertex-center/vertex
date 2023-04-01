85f8245e50b92aa4db58b61492e419ab78c2b0ca
Quentin Guidée <quentin.guidee@gmail.com>
2023-04-01 00:01:39 -0400

    Remove duplicated navigation and replace by localhost website links
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/pages/BayDetailsHome/BayDetailsHome.tsx
+++ src/pages/BayDetailsHome/BayDetailsHome.tsx
@@ -1,30 +1,87 @@
-import { Fragment } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import { Title } from "../../components/Text/Text";
 import Symbol from "../../components/Symbol/Symbol";
 
 import styles from "./BayDetailsHome.module.sass";
-import { bayNavItems } from "../BayDetails/BayDetails";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Horizontal } from "../../components/Layouts/Layouts";
+import Spacer from "../../components/Spacer/Spacer";
+import { getInstance, Instance, route } from "../../backend/backend";
+import classNames from "classnames";
+import {
+    registerSSE,
+    registerSSEEvent,
+    unregisterSSE,
+    unregisterSSEEvent,
+} from "../../backend/sse";
 
 export default function BayDetailsHome() {
     const { uuid } = useParams();
+
+    const [instance, setInstance] = useState<Instance>();
+
+    const fetchInstance = useCallback(() => {
+        getInstance(uuid).then((instance: Instance) => {
+            setInstance(instance);
+        });
+    }, [uuid]);
+
+    useEffect(() => {
+        fetchInstance();
+    }, [fetchInstance, uuid]);
+
+    useEffect(() => {
+        if (uuid === undefined) return;
+
+        const sse = registerSSE(route(`/instance/${uuid}/events`));
+
+        const onStatusChange = (e) => {
+            setInstance((instance) => ({ ...instance, status: e.data }));
+        };
+
+        registerSSEEvent(sse, "status_change", onStatusChange);
+
+        return () => {
+            unregisterSSEEvent(sse, "status_change", onStatusChange);
+
+            unregisterSSE(sse);
+        };
+    }, [uuid]);
 
     return (
         <Fragment>
             <Title>Home</Title>
             <nav className={styles.nav}>
-                {bayNavItems.map((item) => (
-                    <Link
-                        to={`/bay/${uuid}${item.to}`}
-                        className={styles.navItem}
-                    >
-                        <Symbol
-                            className={styles.navItemSymbol}
-                            name={item.symbol}
-                        />
-                        <div>{item.label}</div>
-                    </Link>
-                ))}
+                {instance?.environment
+                    .filter((e) => e.type === "port")
+                    .map((e) => {
+                        console.log(instance.env);
+                        const port = instance.env[e.name] ?? e.default;
+                        const disabled = instance.status !== "running";
+                        const href = `http://localhost:${port}`;
+
+                        return (
+                            <a
+                                href={href}
+                                target="_blank"
+                                rel="noreferrer"
+                                className={classNames({
+                                    [styles.navItem]: true,
+                                    [styles.navItemDisabled]: disabled,
+                                })}
+                            >
+                                <Horizontal>
+                                    <Symbol
+                                        className={styles.navItemSymbol}
+                                        name="public"
+                                    />
+                                    <Spacer />
+                                    <Symbol name="open_in_new" />
+                                </Horizontal>
+                                <div>http://localhost:{port}</div>
+                            </a>
+                        );
+                    })}
             </nav>
         </Fragment>
     );
