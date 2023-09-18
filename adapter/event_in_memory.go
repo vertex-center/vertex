b4880bdc1682cb3982b9a1186f796fa50978eae3
Quentin Guidée <quentin.guidee@gmail.com>
2023-09-18 09:01:26 -0400

    Fix event_in_memory concurrent writes

--- adapter/event_in_memory.go
+++ adapter/event_in_memory.go
@@ -1,29 +1,42 @@
 package adapter
 
 import (
+	"sync"
+
 	"github.com/google/uuid"
 	"github.com/vertex-center/vertex/types"
 )
 
 type EventInMemoryAdapter struct {
-	listeners *map[uuid.UUID]types.Listener
+	listeners      *map[uuid.UUID]types.Listener
+	listenersMutex *sync.RWMutex
 }
 
 func NewEventInMemoryAdapter() types.EventAdapterPort {
 	return &EventInMemoryAdapter{
-		listeners: &map[uuid.UUID]types.Listener{},
+		listeners:      &map[uuid.UUID]types.Listener{},
+		listenersMutex: &sync.RWMutex{},
 	}
 }
 
 func (a *EventInMemoryAdapter) AddListener(l types.Listener) {
+	a.listenersMutex.Lock()
+	defer a.listenersMutex.Unlock()
+
 	(*a.listeners)[l.GetUUID()] = l
 }
 
 func (a *EventInMemoryAdapter) RemoveListener(l types.Listener) {
+	a.listenersMutex.Lock()
+	defer a.listenersMutex.Unlock()
+
 	delete(*a.listeners, l.GetUUID())
 }
 
 func (a *EventInMemoryAdapter) Send(e interface{}) {
+	a.listenersMutex.RLock()
+	defer a.listenersMutex.RUnlock()
+
 	for _, l := range *a.listeners {
 		l.OnEvent(e)
 	}
