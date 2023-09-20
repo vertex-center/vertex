368470c3d0001ecdf1243e1d474bb17ed3c210ca
Quentin Guidée <quentin.guidee@gmail.com>
2023-09-20 15:21:00 -0400

    Fix hardware settings title alignment
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/pages/SettingsHardware/SettingsHardware.tsx
+++ src/pages/SettingsHardware/SettingsHardware.tsx
@@ -1,18 +1,18 @@
-import { Fragment } from "react";
 import { Title } from "../../components/Text/Text";
-
-import styles from "./SettingsHardware.module.sass";
 import Hardware from "../../components/Hardware/Hardware";
 import { useFetch } from "../../hooks/useFetch";
 import { api } from "../../backend/backend";
+
+import styles from "./SettingsHardware.module.sass";
+import { Vertical } from "../../components/Layouts/Layouts";
 
 export default function SettingsHardware() {
     const { data: hardware } = useFetch(api.hardware.get);
 
     return (
-        <Fragment>
+        <Vertical gap={20}>
             <Title className={styles.title}>Hardware</Title>
             <Hardware hardware={hardware} />
-        </Fragment>
+        </Vertical>
     );
 }
