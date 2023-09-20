bd34ffdbca33f25a4f12b83798bc2629df3732be
Quentin Guidée <quentin.guidee@gmail.com>
2023-09-20 00:28:36 -0400

    Simplify instance update UI
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/pages/BayDetailsUpdate/BayDetailsUpdate.tsx
+++ src/pages/BayDetailsUpdate/BayDetailsUpdate.tsx
@@ -1,10 +1,8 @@
-import { SubTitle, Title } from "../../components/Text/Text";
+import { Title } from "../../components/Text/Text";
 import { useParams } from "react-router-dom";
 import useInstance from "../../hooks/useInstance";
 import styles from "./BayDetailsUpdate.module.sass";
-import Button from "../../components/Button/Button";
 import { Vertical } from "../../components/Layouts/Layouts";
-import { Fragment, useState } from "react";
 import { api } from "../../backend/backend";
 import Update, { Updates } from "../../components/Update/Update";
 
@@ -13,30 +11,11 @@ export default function BayDetailsUpdate() {
 
     const { instance, reloadInstance } = useInstance(uuid);
 
-    const [updating, setUpdating] = useState(false);
-
-    // let content: any;
-    // if (instance?.update) {
-    //     content = (
-    //         <Vertical gap={10}>
-    //             <Text>An update is available.</Text>
-    //             <Text>Current: {instance?.update?.current_version}</Text>
-    //             <Text>Latest: {instance?.update?.latest_version}</Text>
-    //         </Vertical>
-    //     );
-    // } else {
-    //     content = (
-    //         <Text className={styles.content}>Everything is up-to-date</Text>
-    //     );
-    // }
-
     const updateVertexIntegration = () => {
-        setUpdating(true);
-        api.instance.update
+        return api.instance.update
             .service(uuid)
             .then(reloadInstance)
-            .catch(console.error)
-            .finally(() => setUpdating(false));
+            .catch(console.error);
     };
 
     return (
@@ -49,26 +28,6 @@ export default function BayDetailsUpdate() {
                     available={instance?.service_update?.available}
                 />
             </Updates>
-            {instance?.service_update?.available && (
-                <Fragment>
-                    <SubTitle className={styles.content}>
-                        Vertex integration
-                    </SubTitle>
-                    <div className={styles.content}>
-                        A new version of the Vertex integration is available for
-                        this instance.
-                    </div>
-                    <div>
-                        <Button
-                            disabled={updating}
-                            rightSymbol="download"
-                            onClick={updateVertexIntegration}
-                        >
-                            Update
-                        </Button>
-                    </div>
-                </Fragment>
-            )}
         </Vertical>
     );
 }
