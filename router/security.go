3bde00327ad1ec991a6f8b6839e2cb9b99375813
Quentin Guidée <quentin.guidee@gmail.com>
2023-09-23 11:11:55 -0400

    Create SSH keys route (fixes #16)

--- router/security.go
+++ router/security.go
@@ -1,22 +1,69 @@
 package router
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/vertex-center/vertex/services"
+	"github.com/vertex-center/vertex/types"
 )
 
 func addSecurityRoutes(r *gin.RouterGroup) {
 	r.GET("/ssh", handleGetSSHKey)
+	r.POST("/ssh", handleAddSSHKey)
 }
 
 // handleGetSSHKey handles the retrieval of the SSH key.
+// Errors can be:
+//   - failed_to_get_ssh_keys: failed to get the SSH keys.
 func handleGetSSHKey(c *gin.Context) {
 	keys, err := sshService.GetAll()
 	if err != nil {
-		_ = c.AbortWithError(500, err)
+		_ = c.AbortWithError(http.StatusInternalServerError, types.APIError{
+			Code:    "failed_to_get_ssh_keys",
+			Message: fmt.Sprintf("failed to get SSH keys: %v", err),
+		})
 		return
 	}
 
 	c.JSON(http.StatusOK, keys)
+}
+
+type addSSHKeyBody struct {
+	AuthorizedKey string `json:"authorized_key"`
+}
+
+// handleAddSSHKey handles the addition of an SSH key.
+// Errors can be:
+//   - failed_to_parse_body: failed to parse the request body.
+//   - failed_to_add_ssh_key: failed to add the SSH key.
+func handleAddSSHKey(c *gin.Context) {
+	var body addSSHKeyBody
+	err := c.BindJSON(&body)
+	if err != nil {
+		_ = c.AbortWithError(http.StatusBadRequest, types.APIError{
+			Code:    "failed_to_parse_body",
+			Message: fmt.Sprintf("failed to parse request body: %v", err),
+		})
+		return
+	}
+
+	err = sshService.Add(body.AuthorizedKey)
+	if err != nil && errors.Is(err, services.ErrInvalidPublicKey) {
+		_ = c.AbortWithError(http.StatusBadRequest, types.APIError{
+			Code:    "invalid_public_key",
+			Message: fmt.Sprintf("error while parsing the public key: %v", err),
+		})
+		return
+	} else if err != nil {
+		_ = c.AbortWithError(http.StatusInternalServerError, types.APIError{
+			Code:    "failed_to_add_ssh_key",
+			Message: fmt.Sprintf("failed to add SSH key: %v", err),
+		})
+		return
+	}
+
+	c.Status(http.StatusCreated)
 }
--- services/ssh.go
+++ services/ssh.go
@@ -1,12 +1,17 @@
 package services
 
 import (
+	"errors"
 	"os"
 	"path"
 
 	"github.com/vertex-center/vertex/pkg/log"
 	"github.com/vertex-center/vertex/types"
 	"golang.org/x/crypto/ssh"
+)
+
+var (
+	ErrInvalidPublicKey = errors.New("invalid key")
 )
 
 type SSHService struct{}
@@ -16,12 +21,10 @@ func NewSSHService() SSHService {
 }
 
 func (s *SSHService) GetAll() ([]types.PublicKey, error) {
-	dir, err := os.UserHomeDir()
+	authorizedKeysPath, err := s.getAuthorizedKeysPath()
 	if err != nil {
 		return nil, err
 	}
-
-	authorizedKeysPath := path.Join(dir, ".ssh", "authorized_keys")
 
 	bytes, err := os.ReadFile(authorizedKeysPath)
 	if err != nil {
@@ -47,4 +50,36 @@ func (s *SSHService) GetAll() ([]types.PublicKey, error) {
 	}
 
 	return keys, nil
+}
+
+// Add adds an SSH key to the authorized keys file. The key must
+// be a valid SSH public key, otherwise ErrInvalidPublicKey is returned.
+func (s *SSHService) Add(authorizedKey string) error {
+	// Check if the key is valid.
+	_, err := ssh.ParsePublicKey([]byte(authorizedKey))
+	if err != nil {
+		return ErrInvalidPublicKey
+	}
+
+	authorizedKeysPath, err := s.getAuthorizedKeysPath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(authorizedKeysPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(authorizedKey + "\n")
+	return err
+}
+
+func (s *SSHService) getAuthorizedKeysPath() (string, error) {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return path.Join(dir, ".ssh", "authorized_keys"), nil
 }
