c4af70f0398efba08c991c9295ca0f2e4b02cbc0
Quentin Guidée <quentin.guidee@gmail.com>
2023-09-30 21:29:52 -0400

    Refactor and add unit tests to the SSH keys

--- services/ssh_kernel.go
+++ services/ssh_kernel.go
@@ -2,11 +2,7 @@ package services
 
 import (
 	"errors"
-	"os"
-	"path"
-	"strings"
 
-	"github.com/vertex-center/vertex/pkg/log"
 	"github.com/vertex-center/vertex/types"
 	"golang.org/x/crypto/ssh"
 )
@@ -15,110 +11,32 @@ var (
 	ErrInvalidPublicKey = errors.New("invalid key")
 )
 
-type SSHKernelService struct {
-	authorizedKeysPath string
+type SshKernelService struct {
+	sshAdapter types.SshAdapterPort
 }
 
-type SSHKernelServiceParams struct {
-	AuthorizedKeysPath string
+func NewSshKernelService(sshAdapter types.SshAdapterPort) SshKernelService {
+	return SshKernelService{
+		sshAdapter: sshAdapter,
+	}
 }
 
-func NewSSHKernelService(params *SSHKernelServiceParams) SSHKernelService {
-	s := SSHKernelService{}
-
-	if params == nil {
-		params = &SSHKernelServiceParams{}
-	}
-
-	s.authorizedKeysPath = params.AuthorizedKeysPath
-	if s.authorizedKeysPath == "" {
-		var err error
-		s.authorizedKeysPath, err = getAuthorizedKeysPath()
-		if err != nil {
-			log.Error(err)
-		}
-	}
-
-	return s
-}
-
-func (s *SSHKernelService) GetAll() ([]types.PublicKey, error) {
-	bytes, err := os.ReadFile(s.authorizedKeysPath)
-	if err != nil && errors.Is(err, os.ErrNotExist) {
-		log.Info("authorized_keys file does not exist")
-		return []types.PublicKey{}, nil
-	} else if err != nil {
-		return nil, err
-	}
-
-	var publicKeys []ssh.PublicKey
-	for len(bytes) > 0 {
-		pubKey, _, _, rest, _ := ssh.ParseAuthorizedKey(bytes)
-		if pubKey != nil {
-			publicKeys = append(publicKeys, pubKey)
-		}
-		bytes = rest
-	}
-
-	keys := []types.PublicKey{}
-	for _, key := range publicKeys {
-		keys = append(keys, types.PublicKey{
-			Type:              key.Type(),
-			FingerprintSHA256: ssh.FingerprintSHA256(key),
-		})
-	}
-
-	return keys, nil
+// GetAll returns all SSH keys from the authorized keys file.
+func (s *SshKernelService) GetAll() ([]types.PublicKey, error) {
+	return s.sshAdapter.GetAll()
 }
 
 // Add adds an SSH key to the authorized keys file. The key must
 // be a valid SSH public key, otherwise ErrInvalidPublicKey is returned.
-func (s *SSHKernelService) Add(authorizedKey string) error {
-	// Check if the key is valid.
+func (s *SshKernelService) Add(authorizedKey string) error {
 	_, _, _, _, err := ssh.ParseAuthorizedKey([]byte(authorizedKey))
 	if err != nil {
 		return ErrInvalidPublicKey
 	}
-
-	file, err := os.OpenFile(s.authorizedKeysPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(authorizedKey + "\n")
-	return err
+	return s.sshAdapter.Add(authorizedKey)
 }
 
 // Delete deletes an SSH key from the authorized keys file.
-func (s *SSHKernelService) Delete(fingerprint string) error {
-	content, err := os.ReadFile(s.authorizedKeysPath)
-	if err != nil {
-		return err
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		key, _, _, _, _ := ssh.ParseAuthorizedKey([]byte(line))
-		if key == nil {
-			continue
-		}
-
-		fingerprintLine := ssh.FingerprintSHA256(key)
-
-		if fingerprintLine == fingerprint {
-			lines = append(lines[:i], lines[i+1:]...)
-			break
-		}
-	}
-
-	return os.WriteFile(s.authorizedKeysPath, []byte(strings.Join(lines, "\n")), 0644)
-}
-
-func getAuthorizedKeysPath() (string, error) {
-	dir, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-	return path.Join(dir, ".ssh", "authorized_keys"), nil
+func (s *SshKernelService) Delete(fingerprint string) error {
+	return s.sshAdapter.Remove(fingerprint)
 }
--- services/ssh_kernel_test.go
+++ services/ssh_kernel_test.go
@@ -1,151 +1,100 @@
 package services
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"errors"
-	"os"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
-	"golang.org/x/crypto/ssh"
+	"github.com/vertex-center/vertex/types"
 )
 
-type SSHKernelServiceTestSuite struct {
+const (
+	key         = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6IPH4bqdhPVQUfdmuisPdQJO6Tv2+a0OZ9qLs6W0W2flxn6/yQmYut02cl0UtNcDtmb4RqNj2ms2v2TeDVSWVZkUR/q4jjZSSljQEpTd3r1YhYrO/GPDNiIUMm5HvZ8qIfBQA6gn9uMT1g6FO53O64ACNr+ItU4gNdr+S44MNJRMxMy6+s/LsFlQjyO2MbPQHQ6HSOgTLrCNiH8NTLA/evekrZ/rmIZrrES2vQvw5pbCDgEOkLZruRSMMFJFStb6tlGoiN/jQpfX51jebDVLZ1/U3SU5+7LNN6DxZYE9w1eCA2G8L8q1PUYju+b4F6IhGA1AYXPaAaR12qRJ4lLeN"
+	fingerprint = "SHA256:eLfsDB1H1SrvT7Bgo9U1i/ATcldIrOqin2H0MGEy5I8"
+)
+
+var (
+	authorizedKeys = []types.PublicKey{
+		{
+			Type:              "ssh-rsa",
+			FingerprintSHA256: "SHA256:eLfsDB1H1SrvT7Bgo9U1i/ATcldIrOqin2H0MGEy5I8",
+		},
+		{
+			Type:              "ssh-rsa",
+			FingerprintSHA256: "SHA256:ubvRPPaAlkFeuFQeC748c43nRPTjaRGxnG9C0j+WlJ0",
+		},
+	}
+)
+
+type SshKernelServiceTestSuite struct {
 	suite.Suite
 
-	service            SSHKernelService
-	authorizedKeysFile *os.File
-
-	// Test data
-	keys           []string
-	authorizedKeys []ssh.PublicKey
-	fingerprints   []string
+	service SshKernelService
+	adapter MockSshAdapter
 }
 
-func TestSSHKernelServiceTestSuite(t *testing.T) {
-	suite.Run(t, new(SSHKernelServiceTestSuite))
+func TestSshKernelServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(SshKernelServiceTestSuite))
 }
 
-func (suite *SSHKernelServiceTestSuite) SetupSuite() {
-	suite.keys = []string{
-		"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6IPH4bqdhPVQUfdmuisPdQJO6Tv2+a0OZ9qLs6W0W2flxn6/yQmYut02cl0UtNcDtmb4RqNj2ms2v2TeDVSWVZkUR/q4jjZSSljQEpTd3r1YhYrO/GPDNiIUMm5HvZ8qIfBQA6gn9uMT1g6FO53O64ACNr+ItU4gNdr+S44MNJRMxMy6+s/LsFlQjyO2MbPQHQ6HSOgTLrCNiH8NTLA/evekrZ/rmIZrrES2vQvw5pbCDgEOkLZruRSMMFJFStb6tlGoiN/jQpfX51jebDVLZ1/U3SU5+7LNN6DxZYE9w1eCA2G8L8q1PUYju+b4F6IhGA1AYXPaAaR12qRJ4lLeN",
-		"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtkVmRevgiIRc7QHahcd01d+0qjtZj1KcY5u25TQW7GomgVuJukdKupnUP2Q1DGo1JjI0OMaIVcEAs4rQgHDAIYovHSeQpkhb3QzJKpS9YUxq/ZWtBQd7cdyRrwAJuT0uR0m52NopEVaaETSIFH6byScRoOAdKgRPwWv5EiHleklOuZCG2/BKq2FtHIb5xb7eAEeMy/5ebu1f4C211/q/Y0AIy/Gp7rJGTDSutTi2UXMQxo3kVDykIIg/xqH2h5IUvYOR8Y+t6f9rbKPcglc+9ygmYHeqrIVmkFzru1sbOOCHlIfv1N53RVp5A9734cHm9u3FzfIPkV+j0tOJ8dhdP",
-	}
-
-	for _, key := range suite.keys {
-		k, _, _, _, _ := ssh.ParseAuthorizedKey([]byte(key))
-		if k == nil {
-			continue
-		}
-		suite.authorizedKeys = append(suite.authorizedKeys, k)
-		suite.fingerprints = append(suite.fingerprints, ssh.FingerprintSHA256(k))
-	}
+func (suite *SshKernelServiceTestSuite) SetupSuite() {
+	suite.adapter = MockSshAdapter{}
+	suite.service = NewSshKernelService(&suite.adapter)
 }
 
-func (suite *SSHKernelServiceTestSuite) SetupTest() {
-	var err error
-
-	suite.authorizedKeysFile, err = os.CreateTemp("", "*_authorized_keys")
-	if err != nil {
-		suite.FailNow(err.Error())
-	}
-
-	for i := range suite.keys {
-		_, err = suite.authorizedKeysFile.WriteString(suite.keys[i] + "\n")
-		if err != nil {
-			suite.FailNow(err.Error())
-		}
-	}
-
-	suite.service = NewSSHKernelService(&SSHKernelServiceParams{
-		AuthorizedKeysPath: suite.authorizedKeysFile.Name(),
-	})
-}
-
-func (suite *SSHKernelServiceTestSuite) TearDownTest() {
-	err := os.Remove(suite.authorizedKeysFile.Name())
-	if err != nil && !errors.Is(err, os.ErrNotExist) {
-		suite.NoError(err)
-	}
-}
-
-func (suite *SSHKernelServiceTestSuite) TestGetAll() {
-	keys, err := suite.service.GetAll()
-	suite.NoError(err)
-	suite.Equal(2, len(keys))
-	for i, key := range keys {
-		suite.Equal("ssh-rsa", key.Type)
-		suite.Equal(suite.fingerprints[i], key.FingerprintSHA256)
-	}
-}
-
-func (suite *SSHKernelServiceTestSuite) TestGetAllInvalidKey() {
-	_, err := suite.authorizedKeysFile.Write([]byte("invalid"))
-	suite.NoError(err)
+func (suite *SshKernelServiceTestSuite) TestGetAll() {
+	suite.adapter.On("GetAll").Return(authorizedKeys, nil)
 
 	keys, err := suite.service.GetAll()
+
 	suite.NoError(err)
-	suite.Equal(2, len(keys))
+	suite.Equal(authorizedKeys, keys)
+	suite.adapter.AssertExpectations(suite.T())
 }
 
-func (suite *SSHKernelServiceTestSuite) TestGetAllNoSuchFile() {
-	suite.authorizedKeysFile.Close()
-	err := os.Remove(suite.service.authorizedKeysPath)
-	suite.NoError(err)
+func (suite *SshKernelServiceTestSuite) TestAdd() {
+	suite.adapter.On("Add", key).Return(nil)
 
-	keys, err := suite.service.GetAll()
+	err := suite.service.Add(key)
+
 	suite.NoError(err)
-	suite.Equal(0, len(keys))
+	suite.adapter.AssertExpectations(suite.T())
 }
 
-func (suite *SSHKernelServiceTestSuite) TestAdd() {
-	publicKey, err := generatePublicKey()
-	if err != nil {
-		suite.FailNow(err.Error())
-	}
+func (suite *SshKernelServiceTestSuite) TestAddInvalidKey() {
+	suite.adapter.On("Add", key).Return(nil)
 
-	err = suite.service.Add(string(publicKey))
-	suite.NoError(err)
-
-	keys, err := suite.service.GetAll()
-	suite.NoError(err)
-	suite.Equal(3, len(keys))
-}
-
-func (suite *SSHKernelServiceTestSuite) TestAddInvalidKey() {
 	err := suite.service.Add("invalid")
+
 	suite.Error(err)
-
-	keys, err := suite.service.GetAll()
-	suite.NoError(err)
-	suite.Equal(2, len(keys))
+	suite.ErrorIsf(err, ErrInvalidPublicKey, "invalid key")
+	suite.adapter.AssertNotCalled(suite.T(), "Add", "invalid")
 }
 
-func (suite *SSHKernelServiceTestSuite) TestDelete() {
-	err := suite.service.Delete(ssh.FingerprintSHA256(suite.authorizedKeys[0]))
-	suite.NoError(err)
+func (suite *SshKernelServiceTestSuite) TestDelete() {
+	suite.adapter.On("Remove", fingerprint).Return(nil)
 
-	keys, err := suite.service.GetAll()
+	err := suite.service.Delete(fingerprint)
+
 	suite.NoError(err)
-	suite.Equal(1, len(keys))
+	suite.adapter.AssertExpectations(suite.T())
 }
 
-func generatePublicKey() ([]byte, error) {
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return nil, err
-	}
+type MockSshAdapter struct {
+	mock.Mock
+}
 
-	err = key.Validate()
-	if err != nil {
-		return nil, err
-	}
+func (m *MockSshAdapter) GetAll() ([]types.PublicKey, error) {
+	args := m.Called()
+	return args.Get(0).([]types.PublicKey), args.Error(1)
+}
 
-	publicKey, err := ssh.NewPublicKey(&key.PublicKey)
-	if err != nil {
-		return nil, err
-	}
+func (m *MockSshAdapter) Add(key string) error {
+	args := m.Called(key)
+	return args.Error(0)
+}
 
-	return ssh.MarshalAuthorizedKey(publicKey), nil
+func (m *MockSshAdapter) Remove(fingerprint string) error {
+	args := m.Called(fingerprint)
+	return args.Error(0)
 }
