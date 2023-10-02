9520302aaa3190ab33a2d759795f3f419a85d25a
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-01 21:43:11 -0400

    Add a fullscreen loading logo
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/components/LoadingLogo/LoadingLogo.tsx
+++ src/components/LoadingLogo/LoadingLogo.tsx
@@ -1,4 +1,19 @@
 import styles from "./LoadingLogo.module.sass";
+import { Vertical } from "../Layouts/Layouts";
+import { SubTitle } from "../Text/Text";
+
+export function FullLoadingLogo({ show }: { show?: boolean }) {
+    if (!show) return null;
+
+    return (
+        <div className={styles.full}>
+            <Vertical gap={10} alignItems="center">
+                <LoadingLogo />
+                <SubTitle>Loading...</SubTitle>
+            </Vertical>
+        </div>
+    );
+}
 
 export default function LoadingLogo() {
     return (
