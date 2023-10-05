7aaa66ce2bc00500fc48d7bd45e66ca09b22bfd9
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-04 20:31:37 -0400

    ErrCode typing

--- types/api/errors.go
+++ types/api/errors.go
@@ -1,66 +1,68 @@
 package api
 
+type ErrCode string
+
 const (
-	ErrFailedToParseBody = "failed_to_parse_body"
+	ErrFailedToParseBody ErrCode = "failed_to_parse_body"
 
-	ErrFailedToInstallUpdates = "failed_to_install_updates"
-	ErrFailedToReloadServices = "failed_to_reload_services"
+	ErrFailedToInstallUpdates ErrCode = "failed_to_install_updates"
+	ErrFailedToReloadServices ErrCode = "failed_to_reload_services"
 
-	ErrFailedToListContainers    = "failed_to_list_containers"
-	ErrFailedToDeleteContainer   = "failed_to_delete_container"
-	ErrFailedToCreateContainer   = "failed_to_create_container"
-	ErrFailedToStartContainer    = "failed_to_start_container"
-	ErrFailedToStopContainer     = "failed_to_stop_container"
-	ErrFailedToRecreateContainer = "failed_to_recreate_container"
-	ErrFailedToGetContainerLogs  = "failed_to_get_container_logs"
-	ErrFailedToWaitContainer     = "failed_to_wait_container"
-	ErrFailedToGetContainerInfo  = "failed_to_get_container_info"
-	ErrFailedToGetImageInfo      = "failed_to_get_image_info"
-	ErrFailedToPullImage         = "failed_to_pull_image"
-	ErrFailedToBuildImage        = "failed_to_build_image"
-	ErrContainerNotFound         = "container_not_found"
+	ErrFailedToListContainers    ErrCode = "failed_to_list_containers"
+	ErrFailedToDeleteContainer   ErrCode = "failed_to_delete_container"
+	ErrFailedToCreateContainer   ErrCode = "failed_to_create_container"
+	ErrFailedToStartContainer    ErrCode = "failed_to_start_container"
+	ErrFailedToStopContainer     ErrCode = "failed_to_stop_container"
+	ErrFailedToRecreateContainer ErrCode = "failed_to_recreate_container"
+	ErrFailedToGetContainerLogs  ErrCode = "failed_to_get_container_logs"
+	ErrFailedToWaitContainer     ErrCode = "failed_to_wait_container"
+	ErrFailedToGetContainerInfo  ErrCode = "failed_to_get_container_info"
+	ErrFailedToGetImageInfo      ErrCode = "failed_to_get_image_info"
+	ErrFailedToPullImage         ErrCode = "failed_to_pull_image"
+	ErrFailedToBuildImage        ErrCode = "failed_to_build_image"
+	ErrContainerNotFound         ErrCode = "container_not_found"
 
-	ErrInstanceUuidInvalid           = "instance_uuid_invalid"
-	ErrInstanceUuidMissing           = "instance_uuid_missing"
-	ErrInstanceNotFound              = "instance_not_found"
-	ErrInstanceAlreadyRunning        = "instance_already_running"
-	ErrInstanceStillRunning          = "instance_still_running"
-	ErrInstanceNotRunning            = "instance_not_running"
-	ErrFailedToGetInstance           = "failed_to_get_instance"
-	ErrFailedToStartInstance         = "failed_to_start_instance"
-	ErrFailedToStopInstance          = "failed_to_stop_instance"
-	ErrFailedToDeleteInstance        = "failed_to_delete_instance"
-	ErrFailedToGetInstanceLogs       = "failed_to_get_logs"
-	ErrFailedToUpdateServiceInstance = "failed_to_update_service_instance"
-	ErrFailedToGetVersions           = "failed_to_get_versions"
-	ErrFailedToSetLaunchOnStartup    = "failed_to_set_launch_on_startup"
-	ErrFailedToSetDisplayName        = "failed_to_set_display_name"
-	ErrFailedToSetDatabase           = "failed_to_set_database"
-	ErrFailedToSetVersion            = "failed_to_set_version"
-	ErrFailedToSetEnv                = "failed_to_set_env"
+	ErrInstanceUuidInvalid           ErrCode = "instance_uuid_invalid"
+	ErrInstanceUuidMissing           ErrCode = "instance_uuid_missing"
+	ErrInstanceNotFound              ErrCode = "instance_not_found"
+	ErrInstanceAlreadyRunning        ErrCode = "instance_already_running"
+	ErrInstanceStillRunning          ErrCode = "instance_still_running"
+	ErrInstanceNotRunning            ErrCode = "instance_not_running"
+	ErrFailedToGetInstance           ErrCode = "failed_to_get_instance"
+	ErrFailedToStartInstance         ErrCode = "failed_to_start_instance"
+	ErrFailedToStopInstance          ErrCode = "failed_to_stop_instance"
+	ErrFailedToDeleteInstance        ErrCode = "failed_to_delete_instance"
+	ErrFailedToGetInstanceLogs       ErrCode = "failed_to_get_logs"
+	ErrFailedToUpdateServiceInstance ErrCode = "failed_to_update_service_instance"
+	ErrFailedToGetVersions           ErrCode = "failed_to_get_versions"
+	ErrFailedToSetLaunchOnStartup    ErrCode = "failed_to_set_launch_on_startup"
+	ErrFailedToSetDisplayName        ErrCode = "failed_to_set_display_name"
+	ErrFailedToSetDatabase           ErrCode = "failed_to_set_database"
+	ErrFailedToSetVersion            ErrCode = "failed_to_set_version"
+	ErrFailedToSetEnv                ErrCode = "failed_to_set_env"
 
-	ErrFailedToCheckForUpdates = "failed_to_check_for_updates"
+	ErrFailedToCheckForUpdates ErrCode = "failed_to_check_for_updates"
 
-	ErrRedirectUuidMissing    = "redirect_uuid_missing"
-	ErrRedirectUuidInvalid    = "redirect_uuid_invalid"
-	ErrFailedToAddRedirect    = "failed_to_add_redirect"
-	ErrFailedToRemoveRedirect = "failed_to_remove_redirect"
+	ErrRedirectUuidMissing    ErrCode = "redirect_uuid_missing"
+	ErrRedirectUuidInvalid    ErrCode = "redirect_uuid_invalid"
+	ErrFailedToAddRedirect    ErrCode = "failed_to_add_redirect"
+	ErrFailedToRemoveRedirect ErrCode = "failed_to_remove_redirect"
 
-	ErrFailedToGetSSHKeys   = "failed_to_get_ssh_keys"
-	ErrFailedToAddSSHKey    = "failed_to_add_ssh_key"
-	ErrFailedToDeleteSSHKey = "failed_to_delete_ssh_key"
-	ErrInvalidPublicKey     = "invalid_public_key"
-	ErrInvalidFingerprint   = "invalid_fingerprint"
+	ErrFailedToGetSSHKeys   ErrCode = "failed_to_get_ssh_keys"
+	ErrFailedToAddSSHKey    ErrCode = "failed_to_add_ssh_key"
+	ErrFailedToDeleteSSHKey ErrCode = "failed_to_delete_ssh_key"
+	ErrInvalidPublicKey     ErrCode = "invalid_public_key"
+	ErrInvalidFingerprint   ErrCode = "invalid_fingerprint"
 
-	ErrServiceNotFound        = "service_not_found"
-	ErrFailedToInstallService = "failed_to_install_service"
+	ErrServiceNotFound        ErrCode = "service_not_found"
+	ErrFailedToInstallService ErrCode = "failed_to_install_service"
 
-	ErrFailedToPatchSettings = "failed_to_patch_settings"
+	ErrFailedToPatchSettings ErrCode = "failed_to_patch_settings"
 )
 
 type Error struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
+	Code    ErrCode `json:"code"`
+	Message string  `json:"message"`
 }
 
 func (e Error) Error() string {
