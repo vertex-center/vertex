980e348c6761575ecbff63bcc9e4c2da86ea9ea8
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-12 09:41:57 -0400

    Fix an issue in the eventbus that may ignore to retry
    In some rare cases, when the same number of listeners are added and removed in the same dispatch, event sending is stopped.

--- types/event_bus.go
+++ types/event_bus.go
@@ -53,15 +53,7 @@ func (b *EventBus) Send(e interface{}) {
 
 	notified := map[uuid.UUID]Listener{}
 	tryCount := 0
-	for len(notified) < len(*b.listeners) {
-		if tryCount > 0 {
-			log.Debug("some listeners were not notified; retrying...", vlog.Any("count", len(*b.listeners)-len(notified)))
-		}
-		if tryCount > 10 {
-			log.Error(errors.New("after 10 retries to send events, there seems to be an issue with the event bus; the issue is probably caused by some listeners that create new listeners that themselves create new listeners, and so on"))
-			break
-		}
-
+	for {
 		b.listenersMutex.RLock()
 		var toNotify []Listener
 		for _, l := range *b.listeners {
@@ -72,6 +64,19 @@ func (b *EventBus) Send(e interface{}) {
 			toNotify = append(toNotify, l)
 		}
 		b.listenersMutex.RUnlock()
+
+		if len(toNotify) == 0 {
+			// all listeners were notified
+			break
+		}
+
+		if tryCount > 0 {
+			log.Debug("some listeners were not notified; retrying...", vlog.Any("count", len(*b.listeners)-len(notified)))
+		}
+		if tryCount > 10 {
+			log.Error(errors.New("after 10 retries to send events, there seems to be an issue with the event bus; the issue is probably caused by some listeners that create new listeners that themselves create new listeners, and so on"))
+			break
+		}
 
 		for _, l := range toNotify {
 			l.OnEvent(e)
