5204fca9b06b85253daf3707fb5d10febefd7c2c
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-15 18:45:30 -0400

    Wrap all types of ports in the same type declaration

--- core/port/adapters.go
+++ core/port/adapters.go
@@ -7,37 +7,39 @@ import (
 	"io"
 )
 
-type BaselinesAdapter interface {
-	// GetLatest returns the latest available Baseline. This
-	// will typically fetch the latest Baseline from a remote source.
-	GetLatest(ctx context.Context, channel types.SettingsUpdatesChannel) (types.Baseline, error)
-}
+type (
+	BaselinesAdapter interface {
+		// GetLatest returns the latest available Baseline. This
+		// will typically fetch the latest Baseline from a remote source.
+		GetLatest(ctx context.Context, channel types.SettingsUpdatesChannel) (types.Baseline, error)
+	}
 
-type DockerAdapter interface {
-	ListContainers() ([]types.Container, error)
-	DeleteContainer(id string) error
-	CreateContainer(options types.CreateContainerOptions) (types.CreateContainerResponse, error)
-	StartContainer(id string) error
-	StopContainer(id string) error
-	InfoContainer(id string) (types.InfoContainerResponse, error)
-	LogsStdoutContainer(id string) (io.ReadCloser, error)
-	LogsStderrContainer(id string) (io.ReadCloser, error)
-	WaitContainer(id string, cond types.WaitContainerCondition) error
-	InfoImage(id string) (types.InfoImageResponse, error)
-	PullImage(options types.PullImageOptions) (io.ReadCloser, error)
-	BuildImage(options types.BuildImageOptions) (types2.ImageBuildResponse, error)
-}
+	DockerAdapter interface {
+		ListContainers() ([]types.Container, error)
+		DeleteContainer(id string) error
+		CreateContainer(options types.CreateContainerOptions) (types.CreateContainerResponse, error)
+		StartContainer(id string) error
+		StopContainer(id string) error
+		InfoContainer(id string) (types.InfoContainerResponse, error)
+		LogsStdoutContainer(id string) (io.ReadCloser, error)
+		LogsStderrContainer(id string) (io.ReadCloser, error)
+		WaitContainer(id string, cond types.WaitContainerCondition) error
+		InfoImage(id string) (types.InfoImageResponse, error)
+		PullImage(options types.PullImageOptions) (io.ReadCloser, error)
+		BuildImage(options types.BuildImageOptions) (types2.ImageBuildResponse, error)
+	}
 
-type SettingsAdapter interface {
-	GetSettings() types.Settings
-	GetNotificationsWebhook() *string
-	SetNotificationsWebhook(webhook *string) error
-	GetChannel() *types.SettingsUpdatesChannel
-	SetChannel(channel *types.SettingsUpdatesChannel) error
-}
+	SettingsAdapter interface {
+		GetSettings() types.Settings
+		GetNotificationsWebhook() *string
+		SetNotificationsWebhook(webhook *string) error
+		GetChannel() *types.SettingsUpdatesChannel
+		SetChannel(channel *types.SettingsUpdatesChannel) error
+	}
 
-type SshAdapter interface {
-	GetAll() ([]types.PublicKey, error)
-	Add(key string) error
-	Remove(fingerprint string) error
-}
+	SshAdapter interface {
+		GetAll() ([]types.PublicKey, error)
+		Add(key string) error
+		Remove(fingerprint string) error
+	}
+)
