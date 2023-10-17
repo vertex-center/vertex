94e1bd72884865c644018b69ae8458a1b5200555
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-17 12:15:30 -0400

    Initialize the library
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vertex Components
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import {defineConfig} from 'vite'
+import {resolve} from "path"
+import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        })
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'lib/index.ts'),
+            name: 'vertex-components',
+            fileName: 'vertex-components'
+        },
+        copyPublicDir: false,
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+        }
+    }
+})
