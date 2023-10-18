c0fb899e57e5541038b3792df8b8200c12b5b273
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-18 17:06:39 -0400

    Remove css injection

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import {defineConfig} from "vite";
-import {resolve} from "path";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import {libInjectCss} from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        libInjectCss(),
         dts({
             insertTypesEntry: true,
             exclude: ["**/*.stories.tsx"],
