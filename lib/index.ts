060bccc0b0703cc9297ae8120b122674f3111b88
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-19 22:30:01 -0400

    Export the theme provider

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,8 @@
 import Button from "./components/Button";
 import Logo from "./components/Logo";
 import MaterialIcon from "./components/MaterialIcon";
+import ThemeProvider from "./theme";
 
 import "./index.sass";
 
-export { Button, Logo, MaterialIcon };
+export { Button, Logo, MaterialIcon, ThemeProvider };
