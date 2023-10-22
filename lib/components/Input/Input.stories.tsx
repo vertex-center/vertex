3e7f49ba960ce26340d1e0e36eca41382ad051cf
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-21 22:57:29 -0400

    Fix Input stories import

--- lib/components/Input/Input.stories.tsx
+++ lib/components/Input/Input.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
-import Input from "./Input.tsx";
+import { Input } from "./Input.tsx";
 
 const meta: Meta<typeof Input> = {
     title: "Components/Input",
