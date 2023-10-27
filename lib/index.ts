4d9f80f385c09be46625b8fa5c357533d19acfaf
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-26 21:36:32 -0400

    Export more components from the library

--- lib/index.ts
+++ lib/index.ts
@@ -3,8 +3,21 @@ import { Checkbox } from "./components/Checkbox/Checkbox";
 import { Input } from "./components/Input/Input";
 import { Logo } from "./components/Logo/Logo";
 import { MaterialIcon } from "./components/MaterialIcon/MaterialIcon";
+import {
+    SelectField,
+    SelectOption,
+} from "./components/SelectField/SelectField";
 import { TextField } from "./components/TextField/TextField";
 
 import "./index.sass";
 
-export { Button, Input, Logo, MaterialIcon, TextField };
+export {
+    Button,
+    Checkbox,
+    Input,
+    Logo,
+    MaterialIcon,
+    SelectField,
+    SelectOption,
+    TextField,
+};
