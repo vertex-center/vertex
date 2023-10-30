8c89c47bdf2ef16e78461cc142746d22bf3894d1
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-30 15:10:02 -0400

    Initialize basic style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Documentation from "./pages/Documentation/Documentation.tsx";
 import Home from "./pages/Home/Home.tsx";
 import "@vertex-center/components/dist/style.css";
+import "./reset.css";
+import "./styles/index.sass";
+import { ThemeContext, ThemeProvider } from "./theme.tsx";
+import cx from "classnames";
 
 const router = createBrowserRouter(
     [
@@ -21,8 +25,20 @@ const router = createBrowserRouter(
     }
 );
 
+function App() {
+    const { theme } = useContext(ThemeContext);
+
+    return (
+        <div className={cx("app", theme)}>
+            <RouterProvider router={router} />
+        </div>
+    );
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
