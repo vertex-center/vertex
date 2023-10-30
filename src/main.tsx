7a1d04e424ed5ab4ef9dc6ff35813d8087b27936
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-30 14:59:46 -0400

    Install @vertex-center/components
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Documentation from "./pages/Documentation/Documentation.tsx";
 import Home from "./pages/Home/Home.tsx";
+import "@vertex-center/components/dist/style.css";
 
 const router = createBrowserRouter(
     [
