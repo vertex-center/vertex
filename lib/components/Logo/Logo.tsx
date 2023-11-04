a999ebb2c3b27c23c24238f8513efec3924c9d02
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-03 23:11:49 -0400

    Allow resizing the Logo

--- lib/components/Logo/Logo.tsx
+++ lib/components/Logo/Logo.tsx
@@ -1,9 +1,11 @@
 import Icon from "../../assets/logo.svg";
 import { ImgHTMLAttributes } from "react";
 
-export type LogoProps = ImgHTMLAttributes<HTMLImageElement>;
+export type LogoProps = ImgHTMLAttributes<HTMLImageElement> & {
+    size?: number;
+};
 
 export function Logo(props: Readonly<LogoProps>) {
-    const { alt = "App Logo", ...others } = props;
-    return <img width={30} src={Icon} alt={alt} {...others} />;
+    const { alt = "App Logo", size = 30, ...others } = props;
+    return <img width={size} src={Icon} alt={alt} {...others} />;
 }
