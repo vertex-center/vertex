6a638e2d25d44eb5e8b7e94c87f656744b0bb4ec
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-04 15:50:37 -0400

    Add mdx support for documentation

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), mdx()],
     base: "./",
 });
