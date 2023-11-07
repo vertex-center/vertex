1690c7e84eaff50800fd8f3c6eea5291b4122021
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-07 09:58:24 -0500

    Document events

--- core/types/events.go
+++ core/types/events.go
@@ -1,17 +1,25 @@
 package types
 
 type (
+	// EventServerStart is dispatched when the server is started.
 	EventServerStart struct {
 		// PostMigrationCommands are commands that should be executed after the server has started.
 		// These are migration commands that cannot be executed before the server has started.
 		PostMigrationCommands []interface{}
 	}
 
+	// EventAppReady is dispatched when the app with the id AppID is ready to be used.
 	EventAppReady struct {
+		// AppID is the id of the app that is ready.
 		AppID string
 	}
 
-	EventServerStop      struct{}
+	// EventServerStop is dispatched when the server is stopped.
+	EventServerStop struct{}
+
+	// EventServerHardReset is dispatched when the server is hard reset. This is used for testing purposes.
 	EventServerHardReset struct{}
-	EventVertexUpdated   struct{}
+
+	// EventVertexUpdated is dispatched when the vertex binary is updated.
+	EventVertexUpdated struct{}
 )
