d3d5bd826c0e9ebe1eb87f91159f4c51ef7a4d7f
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-09 08:39:39 -0500

    Create a story for the Sidebar

---
+++ lib/components/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Sidebar } from "./Sidebar.tsx";
+import { MaterialIcon } from "../../index";
+
+const meta: Meta<typeof Sidebar> = {
+    title: "Components/Sidebar",
+    component: Sidebar,
+    tags: ["autodocs"],
+};
+
+type Story = StoryObj<typeof Sidebar>;
+
+export const Normal: Story = {
+    args: {
+        rootUrl: "/my-app",
+        currentUrl: "/my-app/A",
+    },
+    render: function Render(props) {
+        return (
+            <Sidebar {...props}>
+                <Sidebar.Group title="Group A">
+                    <Sidebar.Item
+                        label="Element A"
+                        icon={<MaterialIcon icon="database" />}
+                        link={{ href: "/app/A" }}
+                    />
+                    <Sidebar.Item
+                        label="Element B"
+                        icon={<MaterialIcon icon="database" />}
+                        link={{ href: "/app/B" }}
+                    />
+                </Sidebar.Group>
+                <Sidebar.Group title="Group B">
+                    <Sidebar.Item
+                        label="Element C"
+                        icon={<MaterialIcon icon="database" />}
+                        link={{ href: "/app/C" }}
+                    />
+                </Sidebar.Group>
+            </Sidebar>
+        );
+    },
+};
+
+export default meta;
