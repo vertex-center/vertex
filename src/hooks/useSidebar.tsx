5e0c05425b12186ba0482304d101cc98e1ddbfd0
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-09 11:10:09 -0500

    Fix useHasSidebar import
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/hooks/useSidebar.tsx
+++ src/hooks/useSidebar.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { createPortal } from "react-dom";
-import { useHasSidebar } from "../../../vertex-components";
+import { useHasSidebar } from "@vertex-center/components";
 
 export const useSidebar = (sidebar: ReactNode) => {
     useHasSidebar(true);
