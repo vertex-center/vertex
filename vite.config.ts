cda4eade9aa2f54257a6e84f50255ba10550a8b5
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-14 20:59:50 -0500

    Add Mdx and Yml support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
+import remarkFrontmatter from "remark-frontmatter";
+import yaml from "@rollup/plugin-yaml";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), mdx()],
+    plugins: [
+        react(),
+        mdx({
+            remarkPlugins: [remarkFrontmatter],
+        }),
+        yaml(),
+    ],
     base: "./",
 });
