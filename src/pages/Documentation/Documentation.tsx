b0354876d52f322f26b571dd9052caef71b551b0
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-15 09:23:33 -0500

    Decrease title variants by one

--- src/pages/Documentation/Documentation.tsx
+++ src/pages/Documentation/Documentation.tsx
@@ -17,12 +17,11 @@ export default function Documentation(props: Props) {
 
     const components = useMemo(
         () => ({
-            h1: (props: any) => <Title {...props} variant="h1" />,
-            h2: (props: any) => <Title {...props} variant="h2" />,
-            h3: (props: any) => <Title {...props} variant="h3" />,
-            h4: (props: any) => <Title {...props} variant="h4" />,
-            h5: (props: any) => <Title {...props} variant="h5" />,
-            h6: (props: any) => <Title {...props} variant="h6" />,
+            h1: (props: any) => <Title {...props} variant="h2" />,
+            h2: (props: any) => <Title {...props} variant="h3" />,
+            h3: (props: any) => <Title {...props} variant="h4" />,
+            h4: (props: any) => <Title {...props} variant="h5" />,
+            h5: (props: any) => <Title {...props} variant="h6" />,
         }),
         []
     );
