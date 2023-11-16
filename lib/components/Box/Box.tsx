c74edbb0d30adbe116e774e1b35efc142e22de90
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-16 08:57:44 -0500

    Change the box icon depending on the type

--- lib/components/Box/Box.tsx
+++ lib/components/Box/Box.tsx
@@ -12,23 +12,28 @@ type BoxProps = HTMLProps<HTMLDivElement> & {
 export default function Box(props: Readonly<BoxProps>) {
     const { className, type, children, ...others } = props;
 
-    let label = "";
+    let label = "",
+        icon = "";
+
     switch (type) {
         case "info":
             label = "Info";
+            icon = "info";
             break;
         case "tip":
             label = "Tip";
+            icon = "lightbulb";
             break;
         case "warning":
             label = "Warning";
+            icon = "warning";
             break;
     }
 
     return (
         <div className={cx("box", `box-${type}`, className)} {...others}>
             <div className="box-header">
-                <MaterialIcon icon="error" />
+                <MaterialIcon icon={icon} />
                 <h1>{label}</h1>
             </div>
             <div className="box-content">{children}</div>
