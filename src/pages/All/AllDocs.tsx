8aaba7a189e85a91f8c1196b0092e9edf65cb11a
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-17 18:37:07 -0500

    Generate AllDocs automatically

--- src/pages/All/AllDocs.tsx
+++ src/pages/All/AllDocs.tsx
@@ -11,6 +11,10 @@ import {
 import styles from "./AllDocs.module.sass";
 import { useNavigate } from "react-router-dom";
 
+const docs = import.meta.glob("/docs/*/doc.json", {
+    eager: true,
+});
+
 export default function AllDocs() {
     useTitle("All documentation");
 
@@ -19,17 +23,29 @@ export default function AllDocs() {
     return (
         <div className={styles.all}>
             <List>
-                <ListItem onClick={() => navigate("/vertex/next/about")}>
-                    <ListIcon>
-                        <Logo />
-                    </ListIcon>
-                    <ListInfo>
-                        <ListTitle>Vertex</ListTitle>
-                        <ListDescription>
-                            The main Vertex documentation.
-                        </ListDescription>
-                    </ListInfo>
-                </ListItem>
+                {
+                    Object.entries(docs).map(([path, doc]: [string, any]) => {
+                        let slug = path.split("/")[2];
+                        if (doc.version) slug = slug + "/" + doc.version;
+                        if (doc.main) slug = slug + "/" + doc.main;
+                        return (
+                            <ListItem
+                                key={slug}
+                                onClick={() => navigate(`/${slug}`)}
+                            >
+                                <ListIcon>
+                                    <Logo />
+                                </ListIcon>
+                                <ListInfo>
+                                    <ListTitle>{doc.title}</ListTitle>
+                                    <ListDescription>
+                                        {doc.description}
+                                    </ListDescription>
+                                </ListInfo>
+                            </ListItem>
+                        );
+                    })
+                }
             </List>
         </div>
     );
