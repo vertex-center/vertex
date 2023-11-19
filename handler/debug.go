bb10b82226a3836e7418a00a499435f2450cd3a7
Quentin Guidée <quentin.guidee@gmail.com>
2023-11-18 22:14:50 -0500

    Add description to the hard reset route

--- handler/debug.go
+++ handler/debug.go
@@ -17,7 +17,8 @@ func NewDebugHandler(debugService port.DebugService) port.DebugHandler {
 
 // docapi begin hard_reset
 // docapi method POST
-// docapi summary Hard reset the server
+// docapi summary Hard reset
+// docapi desc This route allows deleting all the server data, which can be useful for debugging purposes.
 // docapi tags Debug
 // docapi response 204
 // docapi end
