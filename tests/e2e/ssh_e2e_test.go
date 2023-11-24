51087858848b702b7b3c62330a6e154e8cfcdc53
Quentin Guidée <git@arra.cloud>
2023-11-24 08:12:36 -0500

    Test GetSSHUsers() in e2e tests

--- tests/e2e/ssh_e2e_test.go
+++ tests/e2e/ssh_e2e_test.go
@@ -3,9 +3,13 @@
 package e2e
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"github.com/vertex-center/vertex/api"
+	"github.com/vertex-center/vertex/config"
+	"github.com/vertex-center/vertex/pkg/user"
 )
 
 type SshE2ETestSuite struct {
@@ -14,4 +18,29 @@ type SshE2ETestSuite struct {
 
 func TestE2ESshTestSuite(t *testing.T) {
 	suite.Run(t, new(SshE2ETestSuite))
+}
+
+func (suite *SshE2ETestSuite) SetupSuite() {
+	config.Current.Port = "7130"
+	config.Current.PortKernel = "7131"
+}
+
+func (suite *SshE2ETestSuite) TestSsh() {
+	sshClient := api.NewClient()
+
+	ctx := context.Background()
+
+	users, err := sshClient.GetSSHUsers(ctx)
+	suite.Require().NoError(err)
+	suite.Len(users, 2)
+	suite.Equal([]user.User{
+		{
+			Name:    "root",
+			HomeDir: "/",
+		},
+		{
+			Name:    "nexa",
+			HomeDir: "/home/nexa",
+		},
+	}, users)
 }
