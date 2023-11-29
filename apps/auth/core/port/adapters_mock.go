2780757bde7eab1f09d2552fbd88abf1705fb640
Quentin Guidée <git@arra.cloud>
2023-11-29 15:32:18 -0500

    Unit test the AuthService

---
+++ apps/auth/core/port/adapters_mock.go
@@ -0,0 +1,35 @@
+package port
+
+import (
+	"github.com/stretchr/testify/mock"
+	"github.com/vertex-center/vertex/apps/auth/core/types"
+)
+
+type MockAuthAdapter struct {
+	mock.Mock
+}
+
+func (m *MockAuthAdapter) CreateAccount(username string, credentials types.CredentialsArgon2id) error {
+	args := m.Called(username, credentials)
+	return args.Error(0)
+}
+
+func (m *MockAuthAdapter) GetCredentials(login string) ([]types.CredentialsArgon2id, error) {
+	args := m.Called(login)
+	return args.Get(0).([]types.CredentialsArgon2id), args.Error(1)
+}
+
+func (m *MockAuthAdapter) SaveToken(token *types.Token) error {
+	args := m.Called(token)
+	return args.Error(0)
+}
+
+func (m *MockAuthAdapter) RemoveToken(token string) error {
+	args := m.Called(token)
+	return args.Error(0)
+}
+
+func (m *MockAuthAdapter) GetToken(token string) (*types.Token, error) {
+	args := m.Called(token)
+	return args.Get(0).(*types.Token), args.Error(1)
+}
---
+++ apps/auth/core/service/auth_test.go
@@ -0,0 +1,128 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"github.com/vertex-center/vertex/apps/auth/core/port"
+	"github.com/vertex-center/vertex/apps/auth/core/types"
+	"github.com/vertex-center/vertex/config"
+)
+
+type AuthServiceTestSuite struct {
+	suite.Suite
+
+	service  *AuthService
+	adapter  port.MockAuthAdapter
+	testCred types.CredentialsArgon2id
+}
+
+func TestAuthServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(AuthServiceTestSuite))
+}
+
+func (suite *AuthServiceTestSuite) SetupTest() {
+	suite.adapter = port.MockAuthAdapter{}
+	suite.service = NewAuthService(&suite.adapter).(*AuthService)
+	suite.testCred = types.CredentialsArgon2id{
+		Login:       "test_login",
+		Hash:        "N6WEEZ++Gh54U9jqEwSmFAWz9Ls+8iyHar4mOU7M71Y=",
+		Type:        "argon2id",
+		Salt:        "vertex",
+		Iterations:  3,
+		Memory:      12 * 1024,
+		Parallelism: 4,
+		KeyLen:      32,
+	}
+}
+
+func (suite *AuthServiceTestSuite) TestLogin() {
+	suite.adapter.On("GetCredentials", "test_login").Return([]types.CredentialsArgon2id{suite.testCred}, nil)
+	suite.adapter.On("SaveToken", mock.Anything).Return(nil)
+
+	token, err := suite.service.Login("test_login", "test_password")
+	suite.Require().NoError(err)
+	suite.Equal("test_login", token.Username)
+	suite.NotEmpty(token.Token)
+	suite.Len(token.Token, 44)
+	suite.adapter.AssertExpectations(suite.T())
+}
+
+func (suite *AuthServiceTestSuite) TestLoginInvalidLogin() {
+	suite.adapter.On("GetCredentials", "invalid_login").Return([]types.CredentialsArgon2id{}, nil)
+
+	_, err := suite.service.Login("invalid_login", "test_password")
+	suite.Require().ErrorIs(err, types.ErrLoginFailed)
+	suite.adapter.AssertExpectations(suite.T())
+}
+
+func (suite *AuthServiceTestSuite) TestLoginInvalidPassword() {
+	suite.adapter.On("GetCredentials", "test_login").Return([]types.CredentialsArgon2id{suite.testCred}, nil)
+
+	_, err := suite.service.Login("test_login", "invalid_password")
+	suite.Require().ErrorIs(err, types.ErrLoginFailed)
+	suite.adapter.AssertExpectations(suite.T())
+}
+
+func (suite *AuthServiceTestSuite) TestRegister() {
+	suite.adapter.On("CreateAccount", "test_login", suite.testCred).Return(nil)
+	suite.adapter.On("GetCredentials", "test_login").Return([]types.CredentialsArgon2id{suite.testCred}, nil)
+	suite.adapter.On("SaveToken", mock.Anything).Return(nil)
+
+	token, err := suite.service.Register("test_login", "test_password")
+	suite.Require().NoError(err)
+	suite.Equal("test_login", token.Username)
+	suite.NotEmpty(token.Token)
+	suite.Len(token.Token, 44)
+	suite.adapter.AssertExpectations(suite.T())
+}
+
+func (suite *AuthServiceTestSuite) TestRegisterInvalidInput() {
+	_, err := suite.service.Register("", "test_password")
+	suite.Require().ErrorIs(err, types.ErrLoginEmpty)
+
+	_, err = suite.service.Register("test_login", "")
+	suite.Require().ErrorIs(err, types.ErrPasswordEmpty)
+
+	_, err = suite.service.Register("test_login", "short")
+	suite.Require().ErrorIs(err, types.ErrPasswordLength)
+}
+
+func (suite *AuthServiceTestSuite) TestLogout() {
+	suite.adapter.On("RemoveToken", "valid_token").Return(nil)
+
+	err := suite.service.Logout("valid_token")
+	suite.Require().NoError(err)
+	suite.adapter.AssertExpectations(suite.T())
+}
+
+func (suite *AuthServiceTestSuite) TestLogoutInvalidToken() {
+	suite.adapter.On("RemoveToken", "invalid_token").Return(types.ErrTokenInvalid)
+
+	err := suite.service.Logout("invalid_token")
+	suite.Require().ErrorIs(err, types.ErrTokenInvalid)
+	suite.adapter.AssertExpectations(suite.T())
+}
+
+func (suite *AuthServiceTestSuite) TestVerify() {
+	suite.adapter.On("GetToken", "valid_token").Return(&types.Token{Username: "test_login"}, nil)
+
+	err := suite.service.Verify("valid_token")
+	suite.Require().NoError(err)
+	suite.adapter.AssertExpectations(suite.T())
+}
+
+func (suite *AuthServiceTestSuite) TestVerifyMasterKey() {
+	suite.Require().Len(config.Current.MasterApiKey, 44)
+	err := suite.service.Verify(config.Current.MasterApiKey)
+	suite.Require().NoError(err)
+}
+
+func (suite *AuthServiceTestSuite) TestVerifyInvalidToken() {
+	suite.adapter.On("GetToken", "invalid_token").Return(&types.Token{}, types.ErrTokenInvalid)
+
+	err := suite.service.Verify("invalid_token")
+	suite.Require().ErrorIs(err, types.ErrTokenInvalid)
+	suite.adapter.AssertExpectations(suite.T())
+}
