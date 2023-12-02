0414d1d22de625f53d36070a75c006cb9603e936
Quentin Guidée <git@arra.cloud>
2023-12-01 23:05:34 -0500

    useUser: increase again the number of retry since there is a concurrency issue
    Signed-off-by: Quentin Guidée <git@arra.cloud>

--- src/apps/Auth/hooks/useUser.tsx
+++ src/apps/Auth/hooks/useUser.tsx
@@ -6,9 +6,9 @@ export default function useUser(username?: string) {
         queryKey: ["user", username],
         queryFn: api.auth.user().get,
         retry: (failureCount, error) => {
-            // Don't retry if the error was caused by an authentication issue
+            // Don't retry too much if the error was caused by an authentication issue
             // @ts-ignore
-            if (error?.response?.status === 401) return false;
+            if (error?.response?.status === 401) return failureCount < 2;
             return failureCount < 3;
         },
     });
