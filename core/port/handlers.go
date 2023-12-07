d8e129dcc2adf47bec2658195ca5970dcd175cea
Quentin Guidée <git@arra.red>
2023-12-06 21:17:37 -0500

    Remove AuthHandler from vertex code

--- core/port/handlers.go
+++ core/port/handlers.go
@@ -8,15 +8,6 @@ type (
 		Get(c *router.Context)
 	}
 
-	AuthHandler interface {
-		// Login handles the login of a user.
-		Login(c *router.Context)
-		// Register handles the registration of a user.
-		Register(c *router.Context)
-		// Logout handles the logout of a user.
-		Logout(c *router.Context)
-	}
-
 	DebugHandler interface {
 		// HardReset do a hard reset of Vertex.
 		HardReset(c *router.Context)
