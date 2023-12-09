bf2ba9893a733b96512d94f82a54b8224b438cb2
Quentin Guidée <git@arra.red>
2023-12-08 22:09:09 -0500

    Register apps manually in the client
    Signed-off-by: Quentin Guidée <git@arra.red>

--- src/hooks/useApps.tsx
+++ src/hooks/useApps.tsx
@@ -1,11 +1,56 @@
-import { useQuery } from "@tanstack/react-query";
-import { api } from "../backend/api/backend";
-
 export const useApps = () => {
-    const queryApps = useQuery({
-        queryKey: ["apps"],
-        queryFn: api.apps.all,
-    });
-    const { data: apps } = queryApps;
-    return { apps };
+    return {
+        apps: [
+            {
+                id: "auth",
+                name: "Vertex Auth",
+                description: "Authentication app for Vertex",
+                icon: "admin_panel_settings",
+                hidden: true,
+            },
+            {
+                id: "sql",
+                name: "Vertex SQL",
+                description: "Create and manage SQL databases.",
+                icon: "database",
+                hidden: false,
+            },
+            {
+                id: "tunnels",
+                name: "Vertex Tunnels",
+                description: "Create and manage tunnels.",
+                icon: "subway",
+                hidden: false,
+            },
+            {
+                id: "monitoring",
+                name: "Vertex Monitoring",
+                description: "Create and manage containers.",
+                icon: "monitoring",
+                hidden: false,
+            },
+            {
+                id: "containers",
+                name: "Vertex Containers",
+                description: "Create and manage containers.",
+                icon: "deployed_code",
+                hidden: false,
+            },
+            {
+                id: "reverse-proxy",
+                name: "Vertex Reverse Proxy",
+                description: "Redirect traffic to your containers.",
+                icon: "router",
+                hidden: false,
+            },
+            {
+                id: "devtools-service-editor",
+                name: "Vertex Service Editor",
+                description: "Create services for publishing.",
+                icon: "frame_source",
+                category: "devtools",
+                hidden: false,
+            },
+        ],
+    };
 };
