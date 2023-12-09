155ee55a93c3c2987326290ec641866392f58eee
Quentin Guidée <git@arra.red>
2023-12-08 20:13:44 -0500

    types: Use example in about instead of fake values
    Signed-off-by: Quentin Guidée <git@arra.red>

--- core/types/about.go
+++ core/types/about.go
@@ -1,9 +1,9 @@
 package types
 
 type About struct {
-	Version string `json:"version" fake:"{appversion}"`
-	Commit  string `json:"commit"  fake:"{commit}"`
-	Date    string `json:"date"    fake:"{date}"`
-	OS      string `json:"os"      fake:"linux"`
-	Arch    string `json:"arch"    fake:"amd64"`
+	Version string `json:"version" example:"1.2.3"`
+	Commit  string `json:"commit"  example:"cd4ba2876f45775287f426c13adb1868f7c96222"`
+	Date    string `json:"date"    example:"2006-01-02T15:04:05Z07:00"`
+	OS      string `json:"os"      example:"linux"`
+	Arch    string `json:"arch"    example:"amd64"`
 }
