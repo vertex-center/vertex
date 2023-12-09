4a5dba95deeb4c3fa81512124d3acf092c436d0b
Quentin Guidée <git@arra.red>
2023-12-08 20:18:32 -0500

    common/events: Condensed types

--- common/event/events.go
+++ common/event/events.go
@@ -1,31 +1,17 @@
 package event
 
 type (
-	// ServerLoad is dispatched before the server starts.
-	ServerLoad struct{}
-
-	// ServerStart is dispatched when the server is started. This event is
-	// dispatched before the setup.
-	ServerStart struct{}
-
-	// ServerSetupCompleted is dispatched when the server setup is completed.
-	ServerSetupCompleted struct{}
+	ServerLoad           struct{} // ServerLoad is dispatched before the server starts.
+	ServerStart          struct{} // ServerStart is dispatched when the server is started.
+	ServerSetupCompleted struct{} // ServerSetupCompleted is dispatched when the server setup is completed.
+	ServerStop           struct{} // ServerStop is dispatched when the server is stopped.
+	ServerHardReset      struct{} // ServerHardReset is dispatched when the server is hard reset. For testing purposes.
+	VertexUpdated        struct{} // VertexUpdated is dispatched when the vertex binary is updated.
+	AllAppsReady         struct{} // AllAppsReady is dispatched when all apps are ready to be used.
 
 	// AppReady is dispatched when the app with the id AppID is ready to be used.
 	AppReady struct {
 		// AppID is the id of the app that is ready.
 		AppID string
 	}
-
-	// AllAppsReady is dispatched when all apps are ready to be used.
-	AllAppsReady struct{}
-
-	// ServerStop is dispatched when the server is stopped.
-	ServerStop struct{}
-
-	// ServerHardReset is dispatched when the server is hard reset. This is used for testing purposes.
-	ServerHardReset struct{}
-
-	// VertexUpdated is dispatched when the vertex binary is updated.
-	VertexUpdated struct{}
 )
