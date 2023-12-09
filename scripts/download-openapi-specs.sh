5bc2dacabc6b01e4587703cb4fecba401a33923e
Quentin Guidée <git@arra.red>
2023-12-09 18:14:30 -0500

    Update scripts with latest openapi specs

--- scripts/download-openapi-specs.sh
+++ scripts/download-openapi-specs.sh
@@ -2,10 +2,10 @@
 
 git clone https://github.com/vertex-center/openapi.git vertex-openapi
 
-find vertex-openapi/ -type f -name 'openapi.*.dev.yaml' | while read file; do
+find vertex-openapi/ -type f -name 'openapi.*.yaml' | while read file; do
 
     filename=$(basename -- "$file")
-    dirname=$(echo "$filename" | sed -e 's/openapi.//' -e 's/.dev.yaml//')
+    dirname=$(echo "$filename" | sed -e 's/openapi.//' -e 's/.yaml//')
 
     mkdir -p "api/$dirname/next"
     cp "$file" "api/$dirname/next/openapi.yml"
