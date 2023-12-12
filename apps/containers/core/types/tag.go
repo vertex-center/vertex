8392d4ece4715081af166db071f9c786382e9ed0
Quentin Guidée <git@arra.red>
2023-12-12 09:01:59 -0500

    containers: Fix the Tag json and db key

--- apps/containers/core/types/tag.go
+++ apps/containers/core/types/tag.go
@@ -3,7 +3,7 @@ package types
 type (
 	Tags []Tag
 	Tag  struct {
-		ContainerID ContainerID `json:"container_uuid" db:"container_uuid" example:"d1fb743c-f937-4f3d-95b9-1a8475464591"`
-		Tag         string      `json:"tag"            db:"tag"            example:"Vertex SQL"`
+		ContainerID ContainerID `json:"container_id" db:"container_id" example:"d1fb743c-f937-4f3d-95b9-1a8475464591"`
+		Tag         string      `json:"tag"          db:"tag"          example:"Vertex SQL"`
 	}
 )
