7862f19fe48e0806818580039c68d0f1de573557
Quentin Guidée <git@arra.red>
2023-12-15 13:27:15 -0500

    containers: Remove some unused methods

--- apps/containers/core/types/container.go
+++ apps/containers/core/types/container.go
@@ -22,25 +22,23 @@ var (
 
 type (
 	ContainerID struct{ uuid.UUID }
+	Containers  []Container
+	Container   struct {
+		ID              ContainerID `json:"id"                  db:"id"                 example:"1cb8c970-395f-4810-8c9e-e4df35f456e1"`
+		ServiceID       string      `json:"service_id"          db:"service_id"         example:"postgres"`
+		UserID          uuid.UUID   `json:"user_id"             db:"user_id"            example:"596ecff2-ca67-4194-947d-59e90920680f"`
+		Image           string      `json:"image"               db:"image"              example:"postgres"`
+		ImageTag        string      `json:"image_tag,omitempty" db:"image_tag"          example:"latest"`
+		Status          string      `json:"status"              db:"status"             example:"running"`
+		LaunchOnStartup bool        `json:"launch_on_startup"   db:"launch_on_startup"  example:"true"`
+		Name            string      `json:"name"                db:"name"               example:"Postgres"`
+		Description     *string     `json:"description"         db:"description"        example:"An SQL database."`
+		Color           *string     `json:"color"               db:"color"              example:"#336699"`
+		Icon            *string     `json:"icon"                db:"icon"               example:"simpleicons/postgres.svg"`
+		Command         *string     `json:"command,omitempty"   db:"command"            example:"tunnel run"`
 
-	Containers []Container
-	Container  struct {
-		ID              ContainerID `json:"id"                       db:"id"                 example:"1cb8c970-395f-4810-8c9e-e4df35f456e1"`
-		ServiceID       string      `json:"service_id"               db:"service_id"         example:"postgres"`
-		UserID          uuid.UUID   `json:"user_id"                  db:"user_id"            example:"596ecff2-ca67-4194-947d-59e90920680f"`
-		Image           string      `json:"image"                    db:"image"              example:"postgres"`
-		ImageTag        string      `json:"image_tag,omitempty"      db:"image_tag"          example:"latest"`
-		Status          string      `json:"status"                   db:"status"             example:"running"`
-		LaunchOnStartup bool        `json:"launch_on_startup"        db:"launch_on_startup"  example:"true"`
-		Name            string      `json:"name"                     db:"name"               example:"Postgres"`
-		Description     *string     `json:"description"              db:"description"        example:"An SQL database."`
-		Color           *string     `json:"color"                    db:"color"              example:"#336699"`
-		Icon            *string     `json:"icon"                     db:"icon"               example:"simpleicons/postgres.svg"`
-		Command         *string     `json:"command,omitempty"        db:"command"            example:"tunnel run"`
-
-		Databases     map[string]ContainerID `json:"databases,omitempty"`
-		Update        *ContainerUpdate       `json:"update,omitempty"`
-		ServiceUpdate ServiceUpdate          `json:"service_update,omitempty"`
+		Databases map[string]ContainerID `json:"databases,omitempty"`
+		Update    *ContainerUpdate       `json:"update,omitempty"`
 	}
 
 	ContainerSearchQuery struct {
@@ -65,16 +63,9 @@ func NewContainerID() ContainerID  { return ContainerID{uuid.New()} }
 func (ContainerID) Type() string   { return "string" }
 func (ContainerID) Format() string { return "uuid" }
 
-func NewContainer(id ContainerID, serviceID string) Container {
-	return Container{
-		ID:        id,
-		ServiceID: serviceID,
-		Status:    ContainerStatusOff,
-	}
-}
-
 func (i *Container) DockerImageVertexName() string { return "vertex_image_" + i.ID.String() }
 func (i *Container) DockerContainerName() string   { return "VERTEX_CONTAINER_" + i.ID.String() }
+func (i *Container) GetImageNameWithTag() string   { return i.Image + ":" + i.ImageTag }
 
 func (i *Container) IsRunning() bool {
 	return i.Status != ContainerStatusOff && i.Status != ContainerStatusError
@@ -82,8 +73,4 @@ func (i *Container) IsRunning() bool {
 
 func (i *Container) IsBusy() bool {
 	return i.Status == ContainerStatusBuilding || i.Status == ContainerStatusStarting || i.Status == ContainerStatusStopping
-}
-
-func (i *Container) GetImageNameWithTag() string {
-	return i.Image + ":" + i.ImageTag
 }
