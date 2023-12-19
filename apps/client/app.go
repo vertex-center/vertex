054d64a3eeb6ee9698fca58425cd46a0ba19017c
Quentin Guidée <git@arra.red>
2023-12-18 21:29:03 -0500

    client: Host the static files on another URL than the API

--- apps/client/app.go
+++ apps/client/app.go
@@ -1,10 +1,14 @@
 package client
 
 import (
+	"context"
+	"net/http"
 	"os"
 	"path"
+	"time"
 
 	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
 	"github.com/vertex-center/vertex/apps/client/meta"
 	"github.com/vertex-center/vertex/apps/client/updates"
 	"github.com/vertex-center/vertex/common/app"
@@ -12,11 +16,14 @@ import (
 	"github.com/vertex-center/vertex/common/log"
 	"github.com/vertex-center/vertex/common/storage"
 	"github.com/vertex-center/vertex/common/updater"
+	"github.com/vertex-center/vertex/pkg/router"
+	"github.com/vertex-center/vlog"
 	"github.com/wI2L/fizz"
 )
 
 type App struct {
-	ctx *app.Context
+	ctx    *app.Context
+	router *router.Router
 }
 
 func NewApp() *App {
@@ -46,10 +53,33 @@ func (a *App) Meta() appmeta.Meta {
 }
 
 func (a *App) Initialize() error {
+	a.router = router.New(nil,
+		router.WithMiddleware(static.Serve("/", static.LocalFile(path.Join(".", storage.FSPath, "client", "dist"), true))),
+	)
+
+	a.router.GET("/ping", nil, func(ctx *gin.Context) {
+		ctx.Status(http.StatusOK)
+	})
+
+	go func() {
+		log.Info("client app starting", vlog.String("port", "6132"))
+		err := a.router.Start(":6132")
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+		log.Info("client app stopped")
+	}()
+
 	return nil
 }
 
 func (a *App) InitializeRouter(r *fizz.RouterGroup) error {
-	r.Use(static.Serve("/", static.LocalFile(path.Join(".", storage.FSPath, "client", "dist"), true)))
 	return nil
+}
+
+func (a *App) Uninitialize() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	defer cancel()
+	return a.router.Stop(ctx)
 }
