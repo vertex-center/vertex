d3751ac77acfc7f94af3fcf833c87293828ea4af
Quentin Guidée <git@arra.red>
2024-01-11 07:53:51 -0500

    docker: Generate config.js at runtime
    Signed-off-by: Quentin Guidée <git@arra.red>

---
+++ docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+VARS=$(env | grep VERTEX_)                                           # Get all env variables starting with VERTEX_.
+VARS=$(echo "$VARS" | sed -e 's/VERTEX_\(.*\)_ADDR=\(.*\)/\1:"\2"/g') # Transform VARS from VERTEX_XXX_ADDR=yyy to XXX: yyy
+VARS=$(echo "$VARS" | tr '[:upper:]' '[:lower:]')                    # All lowercase
+
+echo "$VARS"
+
+# Write env variables to /usr/share/nginx/html/config.js
+echo "window.api_urls = {" > /usr/share/nginx/html/config.js
+for VAR in $VARS
+do
+    echo "  $VAR," >> /usr/share/nginx/html/config.js
+done
+echo "};" >> /usr/share/nginx/html/config.js
+
+# Start nginx
+exec nginx -g 'daemon off;'
