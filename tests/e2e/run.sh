783b7d437792c05f34fdb4eb39a6c57123fa0047
Quentin Guidée <git@arra.red>
2024-02-02 09:13:29 -0500

    tests: Setup e2e tests with venom

--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
+docker-compose -f docker/bundle.docker-compose.yml -f docker/bundle.docker-compose.build.yml up -d --build
+
+#while ! nc -z localhost 7130; do sleep 1; done
+
 echo "Running e2e tests..."
 
-TAG=vertex-e2e-environment
-
-docker build -t $TAG -f tests/e2e/Dockerfile .
-docker run -it -d -p 7130:6130 -p 7131:6131 $TAG
-
-while ! nc -z localhost 7130; do sleep 1; done
-
-FILES=$(find . -name "*_e2e_test.go")
-
-if go test -tags e2e -v "$FILES"; then
-  EXIT_CODE=0
+if venom run "**/*_test.yml"; then
+    EXIT_CODE=0
 else
-  EXIT_CODE=1
+    EXIT_CODE=1
 fi
 
-docker stop "$(docker ps -q --filter ancestor=$TAG)"
-docker rm "$(docker ps -aq --filter ancestor=$TAG)"
+docker-compose -f docker/bundle.docker-compose.yml -f docker/bundle.docker-compose.build.yml down
 
 exit "$EXIT_CODE"
