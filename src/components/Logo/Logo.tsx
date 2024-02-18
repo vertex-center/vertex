0a8c01c78c829f80e54e3b12ae21403ca08de23a
Quentin Guidée <git@arra.red>
2024-02-17 21:04:07 -0500

    components: Remove devtools from logo
    Signed-off-by: Quentin Guidée <git@arra.red>

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,23 +1,15 @@
 import styles from "./Logo.module.sass";
-import classNames from "classnames";
 
 type Props = {
     name?: string;
     iconOnly?: boolean;
-    devtools?: boolean;
 };
 
-export default function Logo({ name, iconOnly, devtools }: Readonly<Props>) {
+export default function Logo({ name, iconOnly }: Readonly<Props>) {
     return (
-        <div
-            className={classNames({
-                [styles.logo]: true,
-                [styles.logoDev]: devtools,
-            })}
-        >
+        <div className={styles.logo}>
             <img width={30} src="/images/logo.png" alt="Logo" />
             {!iconOnly && <h1>{name ?? "Vertex"}</h1>}
-            {devtools && <code className={styles.tag}>DEVTOOLS</code>}
         </div>
     );
 }
