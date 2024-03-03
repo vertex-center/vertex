cefbcadee69326da650fd7ab6d757ad4c09fd6d7
Quentin Guidée <git@arra.red>
2024-03-03 14:55:08 -0500

    merge docs in the main repository
    Signed-off-by: Quentin Guidée <git@arra.red>

--- docs/scripts/download-openapi-specs.sh
+++ docs/scripts/download-openapi-specs.sh
@@ -7,10 +7,10 @@ find vertex-openapi/ -type f -name 'openapi.*.yaml' | while read file; do
     filename=$(basename -- "$file")
     dirname=$(echo "$filename" | sed -e 's/openapi.//' -e 's/.yaml//')
 
-    mkdir -p "api/$dirname/next"
-    cp "$file" "api/$dirname/next/openapi.yml"
+    mkdir -p "docs/api/$dirname/next"
+    cp "$file" "docs/api/$dirname/next/openapi.yml"
 
-    echo "Moved $filename to api/$dirname/next/openapi.yml"
+    echo "Moved $filename to docs/api/$dirname/next/openapi.yml"
 
 done
 
