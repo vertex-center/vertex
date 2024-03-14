5d951deff00a39278d1d49a6e37a53e4f845dae1
Quentin Guidée <git@arra.red>
2024-03-13 22:37:24 -0400

    feat(docs): refresh homepage UI in grid
    Signed-off-by: Quentin Guidée <git@arra.red>

--- docs/src/pages/All/AllDocs.tsx
+++ docs/src/pages/All/AllDocs.tsx
@@ -1,15 +1,16 @@
 import {
     ListDescription,
-    ListIcon,
     ListInfo,
-    ListItem,
     ListTitle,
     Logo,
-    MaterialIcon,
     Title,
 } from "@vertex-center/components";
 import styles from "./AllDocs.module.sass";
 import { useNavigate } from "react-router-dom";
+import { Grid } from "@vertex-center/components/lib/components/Grid/Grid.tsx";
+import { Card } from "@vertex-center/components/lib/components/Card/Card.tsx";
+import { Vertical } from "@vertex-center/components/lib/components/Layout/Layout.tsx";
+import { Graph } from "@phosphor-icons/react";
 
 const docs = import.meta.glob("/{docs,api}/*/{doc,api}.json", {
     eager: true,
@@ -26,7 +27,7 @@ function DocsCategory({ category, title }: DocsCategoryProps) {
     return (
         <div className={styles.category}>
             <Title variant="h2">{title}</Title>
-            <div className={styles.grid}>
+            <Grid className={styles.grid} columnSize={300}>
                 {Object.entries(docs).map(([path, doc]: [string, any]) => {
                     if (!path.startsWith("/" + category + "/")) return null;
                     let slug = path.split("/")[2];
@@ -34,27 +35,26 @@ function DocsCategory({ category, title }: DocsCategoryProps) {
                     if (doc.version) slug = slug + "/" + doc.version;
                     if (doc.main) slug = slug + "/" + doc.main;
                     return (
-                        <ListItem
-                            key={slug}
-                            onClick={() => navigate(`/${slug}`)}
-                        >
-                            <ListIcon>
-                                {category === "docs" ? (
-                                    <Logo />
-                                ) : (
-                                    <MaterialIcon icon="api" />
-                                )}
-                            </ListIcon>
-                            <ListInfo>
-                                <ListTitle>{doc.title}</ListTitle>
-                                <ListDescription>
-                                    {doc.description}
-                                </ListDescription>
-                            </ListInfo>
-                        </ListItem>
+                        <Card key={slug} onClick={() => navigate(`/${slug}`)}>
+                            <Vertical gap={20}>
+                                <div>
+                                    {category === "docs" ? (
+                                        <Logo />
+                                    ) : (
+                                        <Graph size={32} />
+                                    )}
+                                </div>
+                                <ListInfo>
+                                    <ListTitle>{doc.title}</ListTitle>
+                                    <ListDescription>
+                                        {doc.description}
+                                    </ListDescription>
+                                </ListInfo>
+                            </Vertical>
+                        </Card>
                     );
                 })}
-            </div>
+            </Grid>
         </div>
     );
 }
